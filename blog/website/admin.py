e1b25760ba1d1ad8aec5367b63e9728025f1640a
cesfnt <cesarfnt@outlook.com>
2021-03-10 19:03:42 -0300

    Renderização do template e arquivos de media

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'sub_title', 'full_name',
+                    'categories', 'deleted']
+    search_fields = ['title', 'sub_title']
+    #fields = ('title', 'sub_title')
+
+    #def get_queryset(self, request):
+        #return Post.objects.filter(deleted=True)
+
+admin.site.register(Post, PostAdmin)
