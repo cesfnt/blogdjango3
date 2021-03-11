2d89fb731a622f28a6d903e65c778acd946ee547
cesfnt <cesarfnt@outlook.com>
2021-03-11 20:49:26 -0300

    Adicionando IP e setando debug false

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'h3*umphv8(ml9b0x)@(!i%x-g-l@sz)bz7xf2s4-6*4mu3loa='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '35.223.127.165']
 
 
 # Application definition
